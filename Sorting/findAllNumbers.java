d36bacefda7900b6159669219ecfe0d17c72486f
Shiva Kewat <shivakewat.3537@gmail.com>
2026-03-25 13:47:07 +0530

    Find All Numbers Disappeared in an Array

---
+++ Sorting/findAllNumbers.java
@@ -0,0 +1,43 @@
+// 448. Find All Numbers Disappeared in an Array
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], 
+// return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while(i < nums.length){
+            int correct =  nums[i] - 1;
+            if (nums[i] != nums[correct]){
+                swap(nums,i,correct);
+            }else{
+                i++;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0;index<nums.length;index++){
+            if(nums[index] != index + 1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+
+    }
+    void swap (int[]nums,int first,int second){
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+ 
+
