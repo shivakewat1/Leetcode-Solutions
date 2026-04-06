846e2b63169c7a4a12c033950b7d54a52c580288
Shiva Kewat <shivakewat.3537@gmail.com>
2026-04-06 20:49:28 +0530

    Fixed the code comments

--- Arrays/1295_find_Number_Every_Even_Digits.java
+++ Arrays/1295_find_Number_Every_Even_Digits.java
@@ -1,11 +1,7 @@
 // 1295. Find Numbers with Even Number of Digits
-// Solved
-// Easy
-// Topics
-// premium lock icon
-// Companies
-// Hint
-// Given an array nums of integers, return how many of them contain an even number of digits.
+
+// Given an array nums of integers, return how many of them 
+// contain an even number of digits.
 
 class Solution {
     public int findNumbers(int[] nums) {
